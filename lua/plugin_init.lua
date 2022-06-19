aa4a1af9fdcbf5a334ce86b4fe932f260540c72b
wxh <stormaroon@gmail.com>
2022-06-19 21:48:50 +0800

    Add gitsigns

--- lua/plugin_init.lua
+++ lua/plugin_init.lua
@@ -98,6 +98,9 @@ return packer.startup(function(use)
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
+  -- Git
+  use "lewis6991/gitsigns.nvim"
+
   -- Wakatime
   use 'wakatime/vim-wakatime'
 
