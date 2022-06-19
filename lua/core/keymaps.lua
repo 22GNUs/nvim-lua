ce3bdcf421a035909e8674ee6a0c5ffd53c86e04
wxh <stormaroon@gmail.com>
2022-06-19 10:08:53 +0800

    Add core keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -12,6 +12,33 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+-----------------------------------------------------------
+-- Neovim shortcuts
+-----------------------------------------------------------
+-- Change leader to a comma
+vim.g.mapleader = ','
+
+-- Disable arrow keys
+map('', '<up>', '<nop>')
+map('', '<down>', '<nop>')
+map('', '<left>', '<nop>')
+map('', '<right>', '<nop>')
+
+map('n', '>', 'gt')
+map('n', '<', 'gT')
+
+-- Clear search highlighting with double Esc
+map('n', '<Esc><Esc>', ':nohl<CR>')
+
+-- Move around splits using Ctrl + {h,j,k,l}
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
+
+-- Reload configuration without restart nvim
+map('n', '<leader>r', ':so %<CR>')
+
 return {
   -- export map function so it can be used in other files
   map = map
